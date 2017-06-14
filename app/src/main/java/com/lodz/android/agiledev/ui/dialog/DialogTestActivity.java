6d7c142e53d5392fcc7dd2f531965905d07185e2
lodz <lz.mms@foxmail.com>
2017-06-15 00:04:10 +0800

    【修改】增加弹框测试类

---
+++ app/src/main/java/com/lodz/android/agiledev/ui/dialog/DialogTestActivity.java
@@ -0,0 +1,63 @@
+package com.lodz.android.agiledev.ui.dialog;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.lodz.android.agiledev.R;
+import com.lodz.android.component.base.activity.AbsActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by zhouL on 2017/6/14.
+ */
+
+public class DialogTestActivity extends AbsActivity{
+
+    @BindView(R.id.center_btn)
+    Button mCenterBtn;
+
+    @BindView(R.id.right_btn)
+    Button mRightBtn;
+
+    @BindView(R.id.bottom_btn)
+    Button mBottomBtn;
+
+    @Override
+    protected int getAbsLayoutId() {
+        return R.layout.activity_dialog_test_layout;
+    }
+
+    @Override
+    protected void findViews(Bundle savedInstanceState) {
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void setListeners() {
+        super.setListeners();
+        mCenterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TestCenterDialog dialog = new TestCenterDialog(getContext());
+                dialog.show();
+            }
+        });
+
+        mRightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mBottomBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/lodz/android/agiledev/ui/dialog/TestCenterDialog.java
@@ -0,0 +1,29 @@
+package com.lodz.android.agiledev.ui.dialog;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.lodz.android.agiledev.R;
+import com.lodz.android.component.widget.dialog.BaseDialog;
+
+/**
+ * 测试中间弹框
+ * Created by zhouL on 2017/6/14.
+ */
+
+public class TestCenterDialog extends BaseDialog{
+
+    public TestCenterDialog(@NonNull Context context) {
+        super(context);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.dialog_test_center_layout;
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+}
