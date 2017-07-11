bb8b809c277295bfe638d27806f9b3d1cb343cab
LZ9 <lz.mms@foxmail.com>
2017-07-11 15:01:57 +0800

    添加一个不能左右滑动的ViewPager

---
+++ component/src/main/java/com/lodz/android/component/widget/NoScrollViewPager.java
@@ -0,0 +1,48 @@
+package com.lodz.android.component.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * 不能左右滑动的ViewPager
+ * Created by zhouL on 2016/11/23.
+ */
+public class NoScrollViewPager extends ViewPager {
+
+    /** 是否可以滑动，默认不行 */
+    private boolean isScroll = false;
+
+    public NoScrollViewPager(Context context) {
+        super(context);
+    }
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * 设置是否可以左右滑动
+     * @param isScroll 是否可以滑动
+     */
+    public void setScroll(boolean isScroll) {
+        this.isScroll = isScroll;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (isScroll){
+            return super.onTouchEvent(ev);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (isScroll){
+            return super.onInterceptTouchEvent(ev);
+        }
+        return false;
+    }
+}
