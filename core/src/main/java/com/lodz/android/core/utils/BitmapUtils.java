a8f6c592b19a7417dbf83216a5bd8c3cac2d1c6f
LZ9 <lz.mms@foxmail.com>
2017-07-20 13:52:57 +0800

    合并BitmapUtils和ImageUtils

--- core/src/main/java/com/lodz/android/core/utils/BitmapUtils.java
+++ core/src/main/java/com/lodz/android/core/utils/BitmapUtils.java
@@ -2,8 +2,11 @@ package com.lodz.android.core.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Base64;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -12,6 +15,65 @@ import java.io.InputStream;
  */
 
 public class BitmapUtils {
+
+    /**
+     * bitmap转为base64
+     * @param bitmap 图片
+     */
+    public static String bitmapToBase64Default(Bitmap bitmap) {
+        return bitmapToBase64(bitmap, 70);
+    }
+
+    /**
+     * bitmap转为base64
+     * @param bitmap 图片
+     * @param quality 质量
+     */
+    public static String bitmapToBase64(Bitmap bitmap, int quality) {
+        String result = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            if (bitmap != null) {
+                baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos);
+
+                baos.flush();
+                baos.close();
+
+                byte[] bitmapBytes = baos.toByteArray();
+                result = Base64.encodeToString(bitmapBytes, Base64.DEFAULT);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (baos != null) {
+                    baos.flush();
+                    baos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * base64转为bitmap
+     * @param base64Data base64字符串
+     */
+    public static Bitmap base64ToBitmap(String base64Data) {
+        byte[] bytes = Base64.decode(base64Data, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+
+    /**
+     * base64转为byte[]
+     * @param base64Data base64字符串
+     */
+    public static byte[] base64ToByte(String base64Data) {
+        return Base64.decode(base64Data, Base64.DEFAULT);
+    }
 
     /**
      * 创建bitmap
