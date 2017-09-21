0738f9969f1dedc585c2952387d6c27aabb13afd
lz9 <lz.mms@foxmail.com>
2017-09-21 21:37:54 +0800

    添加照片选择测试类

---
+++ app/src/main/java/com/lodz/android/agiledev/ui/photopicker/PhotoPickerTestActivity.java
@@ -0,0 +1,167 @@
+package com.lodz.android.agiledev.ui.photopicker;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.TextView;
+
+import com.lodz.android.agiledev.R;
+import com.lodz.android.agiledev.ui.main.MainActivity;
+import com.lodz.android.agiledev.ui.splash.CheckDialog;
+import com.lodz.android.component.base.activity.BaseActivity;
+import com.lodz.android.component.widget.base.TitleBarLayout;
+import com.lodz.android.core.utils.AppUtils;
+import com.lodz.android.core.utils.ToastUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+
+/**
+ * 照片选择器测试
+ * Created by zhouL on 2017/9/21.
+ */
+@RuntimePermissions
+public class PhotoPickerTestActivity extends BaseActivity{
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, PhotoPickerTestActivity.class);
+        context.startActivity(starter);
+    }
+
+    /** 选择按钮 */
+    @BindView(R.id.take_btn)
+    TextView mTakeBtn;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_photo_picker_test_layout;
+    }
+
+    @Override
+    protected void findViews(Bundle savedInstanceState) {
+        ButterKnife.bind(this);
+        initTitleBar(getTitleBarLayout());
+    }
+
+    /** 初始化标题栏 */
+    private void initTitleBar(TitleBarLayout titleBarLayout) {
+        titleBarLayout.setTitleName(getIntent().getStringExtra(MainActivity.EXTRA_TITLE_NAME));
+    }
+
+    @Override
+    protected void clickBackBtn() {
+        super.clickBackBtn();
+        finish();
+    }
+
+    @Override
+    protected void clickReload() {
+        super.clickReload();
+        showStatusLoading();
+        PhotoPickerTestActivityPermissionsDispatcher.requestPermissionWithCheck(PhotoPickerTestActivity.this);
+    }
+
+
+    @Override
+    protected void setListeners() {
+        super.setListeners();
+        mTakeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){// 6.0以上的手机对权限进行动态申请
+            PhotoPickerTestActivityPermissionsDispatcher.requestPermissionWithCheck(PhotoPickerTestActivity.this);
+        }else {
+            init();
+        }
+    }
+
+    /** 初始化 */
+    private void init(){
+
+        showStatusCompleted();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PhotoPickerTestActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);//将回调交给代理类处理
+    }
+
+    /** 权限申请成功 */
+    @NeedsPermission({Manifest.permission.CAMERA})// 照相
+    protected void requestPermission() {
+        if (!AppUtils.isPermissionGranted(getContext(), Manifest.permission.CAMERA)){
+            return;
+        }
+        init();
+    }
+
+    /** 被拒绝 */
+    @OnPermissionDenied({Manifest.permission.CAMERA})// 照相
+    protected void onDenied() {
+        ToastUtils.showShort(this, "你拒绝了此权限，该功能不可用");
+        showStatusError();
+    }
+
+    /** 用户拒绝后再次申请前告知用户为什么需要该权限 */
+    @OnShowRationale({Manifest.permission.CAMERA})// 照相
+    protected void showRationaleBeforeRequest(PermissionRequest request) {
+        request.proceed();//请求权限
+    }
+
+    /** 被拒绝并且勾选了不再提醒 */
+    @OnNeverAskAgain({Manifest.permission.CAMERA})// 照相
+    protected void onNeverAskAgain() {
+        ToastUtils.showShort(getContext(), R.string.splash_check_permission_tips);
+        showPermissionCheckDialog();
+        AppUtils.jumpAppDetailSetting(this);
+    }
+
+    /** 显示权限核对弹框 */
+    private void showPermissionCheckDialog() {
+        CheckDialog dialog = new CheckDialog(getContext());
+        dialog.setContentMsg(R.string.splash_check_permission_title);
+        dialog.setPositiveText(R.string.splash_check_permission_confirm, new CheckDialog.Listener() {
+            @Override
+            public void onClick(Dialog dialog) {
+                PhotoPickerTestActivityPermissionsDispatcher.requestPermissionWithCheck(PhotoPickerTestActivity.this);//申请权限
+                dialog.dismiss();
+            }
+        });
+        dialog.setNegativeText(R.string.splash_check_permission_unconfirmed, new CheckDialog.Listener() {
+            @Override
+            public void onClick(Dialog dialog) {
+                AppUtils.jumpAppDetailSetting(getContext());
+            }
+        });
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                finish();
+            }
+        });
+        dialog.show();
+    }
+}
