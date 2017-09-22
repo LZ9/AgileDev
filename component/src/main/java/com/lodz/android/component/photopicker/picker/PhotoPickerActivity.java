09529817d8fc26f9cb83925fd17e454425dae3c3
LZ9 <lz.mms@foxmail.com>
2017-09-22 16:39:35 +0800

    增加photoview

---
+++ component/src/main/java/com/lodz/android/component/photopicker/picker/PhotoPickerActivity.java
@@ -0,0 +1,8 @@
+package com.lodz.android.component.photopicker.picker;
+
+/**
+ * Created by zhouL on 2017/9/22.
+ */
+
+public class PhotoPickerActivity {
+}
---
+++ component/src/main/java/com/lodz/android/component/photopicker/preview/PhotoPreviewActivity.java
@@ -0,0 +1,26 @@
+package com.lodz.android.component.photopicker.preview;
+
+import android.os.Bundle;
+
+import com.lodz.android.component.R;
+import com.lodz.android.component.base.activity.AbsActivity;
+
+/**
+ * 图片预览页面
+ * Created by zhouL on 2017/9/22.
+ */
+
+public class PhotoPreviewActivity extends AbsActivity{
+
+
+
+    @Override
+    protected int getAbsLayoutId() {
+        return R.layout.component_activity_preview_layout;
+    }
+
+    @Override
+    protected void findViews(Bundle savedInstanceState) {
+
+    }
+}
