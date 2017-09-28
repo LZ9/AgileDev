260639598220a7bb065530a2197c07e40ce7ff65
LZ9 <lz.mms@foxmail.com>
2017-09-28 14:06:04 +0800

    Rx帮助类添加获取异常提示语方法

--- component/src/main/java/com/lodz/android/component/rx/status/ResponseStatus.java
+++ component/src/main/java/com/lodz/android/component/rx/status/ResponseStatus.java
@@ -9,4 +9,6 @@ public interface ResponseStatus {
     /** 数据是否返回成功 */
     boolean isSuccess();
 
+    /** 获取提示语信息 */
+    String getMsg();
 }
--- component/src/main/java/com/lodz/android/component/rx/utils/RxUtils.java
+++ component/src/main/java/com/lodz/android/component/rx/utils/RxUtils.java
@@ -1,5 +1,10 @@
 package com.lodz.android.component.rx.utils;
 
+import android.text.TextUtils;
+
+import com.lodz.android.component.rx.exception.DataException;
+import com.lodz.android.component.rx.exception.RxException;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
@@ -21,5 +26,26 @@ public class RxUtils {
             }
         };
     }
+
+    /**
+     * 获取异常的提示语（配合订阅者使用）
+     * @param e 异常
+     * @param isNetwork 是否网络异常
+     * @param defaultTips 默认提示语
+     */
+    public static String getExceptionTips(Throwable e, boolean isNetwork, String defaultTips){
+        if (isNetwork && e instanceof RxException) {
+            RxException exception = (RxException) e;
+            return exception.getErrorMsg();
+        }
+        if (e instanceof DataException){
+            DataException dataException = (DataException) e;
+            if (dataException.getData() != null && !TextUtils.isEmpty(dataException.getData().getMsg())){
+                return dataException.getData().getMsg();
+            }
+        }
+        return defaultTips;
+    }
+
 
 }
