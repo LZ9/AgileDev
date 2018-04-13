43ee975c558b09be51990a24b374c99289f9b3dd
LZ9 <lz.mms@foxmail.com>
2018-04-13 17:45:58 +0800

    日志打印增加分段打印方法

--- core/src/main/java/com/lodz/android/core/log/PrintLog.java
+++ core/src/main/java/com/lodz/android/core/log/PrintLog.java
@@ -1,13 +1,27 @@
 package com.lodz.android.core.log;
 
+import android.support.annotation.IntDef;
 import android.text.TextUtils;
 import android.util.Log;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * 日志打印
  * Created by zhouL on 2016/11/17.
  */
 public class PrintLog {
+
+    private static final int LOG_I = 0;
+    private static final int LOG_V = 1;
+    private static final int LOG_D = 2;
+    private static final int LOG_W = 3;
+    private static final int LOG_E = 4;
+
+    @IntDef({LOG_I, LOG_V, LOG_D, LOG_W, LOG_E})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface LogType {}
 
     /** 是否打印日志 */
     private static boolean sIsPrint = true;
@@ -26,6 +40,15 @@ public class PrintLog {
         }
     }
 
+    public static void is(String tag, String msg) {
+        if (sIsPrint) {
+            if (TextUtils.isEmpty(msg)) {
+                Log.i(tag, "NULL");
+            } else {
+                logSegmented(LOG_I, tag, msg);
+            }
+        }
+    }
 
     public static void v(String tag, String msg) {
         if (sIsPrint) {
@@ -33,6 +56,16 @@ public class PrintLog {
                 Log.v(tag, "NULL");
             } else {
                 Log.v(tag, msg);
+            }
+        }
+    }
+
+    public static void vs(String tag, String msg) {
+        if (sIsPrint) {
+            if (TextUtils.isEmpty(msg)) {
+                Log.v(tag, "NULL");
+            } else {
+                logSegmented(LOG_V, tag, msg);
             }
         }
     }
@@ -47,12 +80,32 @@ public class PrintLog {
         }
     }
 
+    public static void ds(String tag, String msg) {
+        if (sIsPrint) {
+            if (TextUtils.isEmpty(msg)) {
+                Log.d(tag, "NULL");
+            } else {
+                logSegmented(LOG_D, tag, msg);
+            }
+        }
+    }
+
     public static void w(String tag, String msg) {
         if (sIsPrint) {
             if (TextUtils.isEmpty(msg)) {
                 Log.w(tag, "NULL");
             } else {
                 Log.w(tag, msg);
+            }
+        }
+    }
+
+    public static void ws(String tag, String msg) {
+        if (sIsPrint) {
+            if (TextUtils.isEmpty(msg)) {
+                Log.w(tag, "NULL");
+            } else {
+                logSegmented(LOG_W, tag, msg);
             }
         }
     }
@@ -67,6 +120,16 @@ public class PrintLog {
         }
     }
 
+    public static void es(String tag, String msg) {
+        if (sIsPrint) {
+            if (TextUtils.isEmpty(msg)) {
+                Log.e(tag, "NULL");
+            } else {
+                logSegmented(LOG_E, tag, msg);
+            }
+        }
+    }
+
     public static void e(String tag, String msg, Throwable t) {
         if (sIsPrint) {
             if (TextUtils.isEmpty(msg)) {
@@ -74,6 +137,48 @@ public class PrintLog {
             } else {
                 Log.e(tag, msg, t);
             }
+        }
+    }
+
+    private static void logSegmented(@LogType int type, String tag, String log) {
+        if (TextUtils.isEmpty(log) || log.length() < 3000){
+            logByType(type, tag, log);
+            return;
+        }
+        int index = (int) Math.ceil(log.length() / 3000.0);
+        for (int i = 0; i < index; i++){
+            int start = i * 3000;
+            int end = 3000 + i * 3000;
+            if (end >= log.length()){
+                end = log.length();
+            }
+            logByType(type, tag, log.substring(start, end));
+            if (end == log.length()){
+                return;
+            }
+        }
+    }
+
+    private static void logByType(@LogType int type, String tag, String log) {
+        switch (type){
+            case LOG_I:
+                Log.i(tag, log);
+                break;
+            case LOG_V:
+                Log.v(tag, log);
+                break;
+            case LOG_D:
+                Log.d(tag, log);
+                break;
+            case LOG_W:
+                Log.w(tag, log);
+                break;
+            case LOG_E:
+                Log.e(tag, log);
+                break;
+            default:
+                Log.i(tag, log);
+                break;
         }
     }
 
