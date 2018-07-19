f78ce9bc0d8bf5bbc82618edfb80788167961808
LZ9 <lz.mms@foxmail.com>
2018-07-19 16:34:25 +0800

    优化背景选择器帮助类

--- core/src/main/java/com/lodz/android/core/utils/SelectorUtils.java
+++ core/src/main/java/com/lodz/android/core/utils/SelectorUtils.java
@@ -24,7 +24,7 @@ public class SelectorUtils {
      * @param pressed 按压背景
      */
     public static StateListDrawable createBgPressedDrawable(Context context, @DrawableRes int normal, @DrawableRes int pressed){
-        return createBgSelectorDrawable(context, normal, pressed, -1, -1, -1);
+        return createBgSelectorDrawable(context, normal, pressed, 0, 0, 0);
     }
 
     /**
@@ -35,7 +35,7 @@ public class SelectorUtils {
      * @param unable 不可用背景
      */
     public static StateListDrawable createBgPressedUnableDrawable(Context context, @DrawableRes int normal, @DrawableRes int pressed, @DrawableRes int unable){
-        return createBgSelectorDrawable(context, normal, pressed, unable, -1, -1);
+        return createBgSelectorDrawable(context, normal, pressed, unable, 0, 0);
     }
 
     /**
@@ -45,7 +45,7 @@ public class SelectorUtils {
      * @param selected 选中背景
      */
     public static StateListDrawable createBgSelectedDrawable(Context context, @DrawableRes int normal, @DrawableRes int selected){
-        return createBgSelectorDrawable(context, normal, -1, -1, selected, -1);
+        return createBgSelectorDrawable(context, normal, 0, 0, selected, 0);
     }
 
     /**
@@ -83,7 +83,7 @@ public class SelectorUtils {
      * @param pressed 按压背景
      */
     public static StateListDrawable createBgPressedColor(Context context, @ColorRes int normal, @ColorRes int pressed){
-        return createBgSelectorColor(context, normal, pressed, -1, -1, -1);
+        return createBgSelectorColor(context, normal, pressed, 0, 0, 0);
     }
 
     /**
@@ -94,7 +94,7 @@ public class SelectorUtils {
      * @param unable 不可用背景
      */
     public static StateListDrawable createBgPressedUnableColor(Context context, @ColorRes int normal, @ColorRes int pressed, @ColorRes int unable){
-        return createBgSelectorColor(context, normal, pressed, unable, -1, -1);
+        return createBgSelectorColor(context, normal, pressed, unable, 0, 0);
     }
 
     /**
@@ -104,45 +104,45 @@ public class SelectorUtils {
      * @param selected 选中背景
      */
     public static StateListDrawable createBgSelectedColor(Context context, @ColorRes int normal, @ColorRes int selected){
-        return createBgSelectorColor(context, normal, -1, -1, selected, -1);
+        return createBgSelectorColor(context, normal, 0, 0, selected, 0);
     }
 
     /**
      * 创建背景选择器
      * @param context 上下文
-     * @param normal 正常背景（不需要可传-1）
-     * @param pressed 按压背景（不需要可传-1）
-     * @param unable 不可用背景（不需要可传-1）
-     * @param selected 选中背景（不需要可传-1）
-     * @param focused 获取焦点背景（不需要可传-1）
+     * @param normal 正常背景（不需要可传0）
+     * @param pressed 按压背景（不需要可传0）
+     * @param unable 不可用背景（不需要可传0）
+     * @param selected 选中背景（不需要可传0）
+     * @param focused 获取焦点背景（不需要可传0）
      */
     @SuppressLint("ResourceType")
     public static StateListDrawable createBgSelectorDrawable(Context context, @DrawableRes int normal, @DrawableRes int pressed,
             @DrawableRes int unable, @DrawableRes int selected, @DrawableRes int focused){
-        return createBgSelectorDrawable(normal > 0 ? ContextCompat.getDrawable(context, normal) : null,
-                pressed > 0 ? ContextCompat.getDrawable(context, pressed) : null,
-                unable > 0 ? ContextCompat.getDrawable(context, unable) : null,
-                selected > 0 ? ContextCompat.getDrawable(context, selected) : null,
-                focused > 0 ? ContextCompat.getDrawable(context, focused) : null);
+        return createBgSelectorDrawable(normal != 0 ? ContextCompat.getDrawable(context, normal) : null,
+                pressed != 0 ? ContextCompat.getDrawable(context, pressed) : null,
+                unable != 0 ? ContextCompat.getDrawable(context, unable) : null,
+                selected != 0 ? ContextCompat.getDrawable(context, selected) : null,
+                focused != 0 ? ContextCompat.getDrawable(context, focused) : null);
     }
 
     /**
      * 创建背景选择器
      * @param context 上下文
-     * @param normal 正常背景（不需要可传-1）
-     * @param pressed 按压背景（不需要可传-1）
-     * @param unable 不可用背景（不需要可传-1）
-     * @param selected 选中背景（不需要可传-1）
-     * @param focused 获取焦点背景（不需要可传-1）
+     * @param normal 正常背景（不需要可传0）
+     * @param pressed 按压背景（不需要可传0）
+     * @param unable 不可用背景（不需要可传0）
+     * @param selected 选中背景（不需要可传0）
+     * @param focused 获取焦点背景（不需要可传0）
      */
     @SuppressLint("ResourceType")
     public static StateListDrawable createBgSelectorColor(Context context, @ColorRes int normal, @ColorRes int pressed,
             @ColorRes int unable, @ColorRes int selected, @ColorRes int focused){
-        return createBgSelectorDrawable(normal > 0 ? DrawableUtils.createColorDrawable(context, normal) : null,
-                pressed > 0 ? DrawableUtils.createColorDrawable(context, pressed) : null,
-                unable > 0 ? DrawableUtils.createColorDrawable(context, unable) : null,
-                selected > 0 ? DrawableUtils.createColorDrawable(context, selected) : null,
-                focused > 0 ? DrawableUtils.createColorDrawable(context, focused) : null);
+        return createBgSelectorDrawable(normal != 0 ? DrawableUtils.createColorDrawable(context, normal) : null,
+                pressed != 0 ? DrawableUtils.createColorDrawable(context, pressed) : null,
+                unable != 0 ? DrawableUtils.createColorDrawable(context, unable) : null,
+                selected != 0 ? DrawableUtils.createColorDrawable(context, selected) : null,
+                focused != 0 ? DrawableUtils.createColorDrawable(context, focused) : null);
     }
 
     /**
@@ -179,7 +179,7 @@ public class SelectorUtils {
      * @param pressed 按压颜色
      */
     public static ColorStateList createTxPressedColor(@ColorInt int normal, @ColorInt int pressed){
-        return createTxSelectorColor(normal, pressed, -1, -1, -1);
+        return createTxSelectorColor(normal, pressed, 0, 0, 0);
     }
 
     /**
@@ -189,7 +189,7 @@ public class SelectorUtils {
      * @param unable 不可用颜色
      */
     public static ColorStateList createTxPressedUnableColor(@ColorInt int normal, @ColorInt int pressed, @ColorInt int unable){
-        return createTxSelectorColor(normal, pressed, unable, -1, -1);
+        return createTxSelectorColor(normal, pressed, unable, 0, 0);
     }
 
     /**
@@ -198,7 +198,7 @@ public class SelectorUtils {
      * @param selected 选中颜色
      */
     public static ColorStateList createTxSelectedColor(@ColorInt int normal, @ColorInt int selected){
-        return createTxSelectorColor(normal, -1, -1, selected, -1);
+        return createTxSelectorColor(normal, 0, 0, selected, 0);
     }
 
     /**
@@ -208,7 +208,7 @@ public class SelectorUtils {
      * @param pressed 按压颜色
      */
     public static ColorStateList createTxPressedColor(Context context, @ColorRes int normal, @ColorRes int pressed){
-        return createTxSelectorColor(context, normal, pressed, -1, -1, -1);
+        return createTxSelectorColor(context, normal, pressed, 0, 0, 0);
     }
 
     /**
@@ -219,7 +219,7 @@ public class SelectorUtils {
      * @param unable 不可用颜色
      */
     public static ColorStateList createTxPressedUnableColor(Context context, @ColorRes int normal, @ColorRes int pressed, @ColorRes int unable){
-        return createTxSelectorColor(context, normal, pressed, unable, -1, -1);
+        return createTxSelectorColor(context, normal, pressed, unable, 0, 0);
     }
 
     /**
@@ -229,34 +229,34 @@ public class SelectorUtils {
      * @param selected 选中颜色
      */
     public static ColorStateList createTxSelectedColor(Context context, @ColorRes int normal, @ColorRes int selected){
-        return createTxSelectorColor(context, normal, -1, -1, selected, -1);
+        return createTxSelectorColor(context, normal, 0, 0, selected, 0);
     }
 
     /**
      * 创建文字颜色选择器
-     * @param normal 正常颜色（不需要可传-1）
-     * @param pressed 按压颜色（不需要可传-1）
-     * @param unable 不可用颜色（不需要可传-1）
-     * @param selected 选中颜色（不需要可传-1）
-     * @param focused 获取焦点颜色（不需要可传-1）
+     * @param normal 正常颜色（不需要可传0）
+     * @param pressed 按压颜色（不需要可传0）
+     * @param unable 不可用颜色（不需要可传0）
+     * @param selected 选中颜色（不需要可传0）
+     * @param focused 获取焦点颜色（不需要可传0）
      */
     @SuppressLint("ResourceType")
     public static ColorStateList createTxSelectorColor(Context context, @ColorRes int normal, @ColorRes int pressed,
                                                        @ColorRes int unable, @ColorRes int selected, @ColorRes int focused){
-        return createTxSelectorColor(normal > 0 ? ContextCompat.getColor(context, normal) : -1,
-                pressed > 0 ? ContextCompat.getColor(context, pressed) : -1,
-                unable > 0 ? ContextCompat.getColor(context, unable) : -1,
-                selected > 0 ? ContextCompat.getColor(context, selected) : -1,
-                focused > 0 ? ContextCompat.getColor(context, focused) : -1);
+        return createTxSelectorColor(normal != 0 ? ContextCompat.getColor(context, normal) : 0,
+                pressed != 0 ? ContextCompat.getColor(context, pressed) : 0,
+                unable != 0 ? ContextCompat.getColor(context, unable) : 0,
+                selected != 0 ? ContextCompat.getColor(context, selected) : 0,
+                focused != 0 ? ContextCompat.getColor(context, focused) : 0);
     }
 
     /**
      * 创建文字颜色选择器
-     * @param normal 正常颜色（不需要可传-1）
-     * @param pressed 按压颜色（不需要可传-1）
-     * @param unable 不可用颜色（不需要可传-1）
-     * @param selected 选中颜色（不需要可传-1）
-     * @param focused 获取焦点颜色（不需要可传-1）
+     * @param normal 正常颜色（不需要可传0）
+     * @param pressed 按压颜色（不需要可传0）
+     * @param unable 不可用颜色（不需要可传0）
+     * @param selected 选中颜色（不需要可传0）
+     * @param focused 获取焦点颜色（不需要可传0）
      */
     public static ColorStateList createTxSelectorColor(@ColorInt int normal, @ColorInt int pressed,
             @ColorInt int unable, @ColorInt int selected, @ColorInt int focused){
